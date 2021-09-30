e65c0ffcf696c0fc256bf7f24f223637037efdaa
Christian Tischer <christian.tischer@embl.de>
2021-09-30 17:00:23 +0200

    Change to UTF-32 encoding

--- src/main/java/org/janelia/saalfeldlab/n5/UnicodeArrayDataBlock.java
+++ src/main/java/org/janelia/saalfeldlab/n5/UnicodeArrayDataBlock.java
@@ -6,19 +6,18 @@ import java.nio.charset.Charset;
 public class UnicodeArrayDataBlock extends AbstractDataBlock<String[]>
 {
 
-	private final int stringLength;
+	private final int numBytesPerString;
 
-	public UnicodeArrayDataBlock( final int[] size, final long[] gridPosition, final String[] data, int stringLength ) {
+	public UnicodeArrayDataBlock( final int[] size, final long[] gridPosition, final String[] data, int numCharsPerString ) {
 		super(size, gridPosition, data);
-		this.stringLength = stringLength;
+		this.numBytesPerString = numCharsPerString * 4;
 	}
 
 	@Override
 	public ByteBuffer toByteBuffer() {
 
-		final ByteBuffer buffer = ByteBuffer.allocate(data.length * stringLength);
-		char[] charArray = stringArrayToCharArray( data );
-		buffer.asCharBuffer().put(charArray);
+		final ByteBuffer buffer = ByteBuffer.allocate(data.length * numBytesPerString);
+		buffer.asCharBuffer().put(new char[data.length * numBytesPerString]);
 		return buffer;
 	}
 
@@ -35,9 +34,9 @@ public class UnicodeArrayDataBlock extends AbstractDataBlock<String[]>
 	public void readData(final ByteBuffer buffer) {
 		for ( int i = 0; i < data.length; i++ )
 		{
-			final byte[] chars = new byte[ stringLength ];
+			final byte[] chars = new byte[ numBytesPerString ];
 			buffer.get( chars );
-			data[ i ] = new String( chars, Charset.forName("UTF-8"));
+			data[ i ] = new String( chars, Charset.forName("UTF-32"));
 		}
 	}
 
